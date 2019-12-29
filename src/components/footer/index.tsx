dae72290be95a31e106a8fb7003d76ed3f24f36f
lu4ezar <misha-shisha@mail.ru>
2019-12-30 00:30:25 +0700

    removed "return" from Footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,18 +3,16 @@ import Octicon, { MarkGithub } from "@primer/octicons-react";
 import { Link } from "../repo/styled";
 import { FooterDiv, TextLink } from "./styled";
 
-const Footer = () => {
-  return (
-    <FooterDiv>
-      <TextLink>https://github.com/lu4ezar/github-search-graphql</TextLink>
-      <Link
-        href="https://github.com/lu4ezar/github-search-graphql"
-        title="view src on Github"
-      >
-        <Octicon icon={MarkGithub} verticalAlign="middle" size="large" />
-      </Link>
-    </FooterDiv>
-  );
-};
+const Footer = () => (
+  <FooterDiv>
+    <TextLink>https://github.com/lu4ezar/github-search-graphql</TextLink>
+    <Link
+      href="https://github.com/lu4ezar/github-search-graphql"
+      title="view src on Github"
+    >
+      <Octicon icon={MarkGithub} verticalAlign="middle" size="medium" />
+    </Link>
+  </FooterDiv>
+);
 
 export default Footer;
