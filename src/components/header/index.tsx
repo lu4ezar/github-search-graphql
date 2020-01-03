066317c90e206bd5009eb11e1354da6338bc5579
lu4ezar <misha-shisha@mail.ru>
2020-01-03 16:23:04 +0700

    removed redundant class from Header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import HeaderDiv from "./styled";
 
-const Header = () => <HeaderDiv className="header">GITHUB SEARCH</HeaderDiv>;
+const Header = () => <HeaderDiv>GITHUB SEARCH</HeaderDiv>;
 
 export default Header;
