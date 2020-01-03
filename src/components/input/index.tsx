dfb8907e097ab95453e0412564cdd96b81fb34cf
lu4ezar <misha-shisha@mail.ru>
2020-01-03 16:35:08 +0700

    removed class name variable

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,24 +1,21 @@
 import React from "react";
 import { InputDiv, Close } from "./styled";
 
-const Input = ({ searchString, handleInput, clearInput }: any) => {
-  const visibility = searchString ? "show" : "";
-  return (
-    <InputDiv>
-      <label htmlFor="search">
-        <input
-          id="search"
-          type="search"
-          onChange={handleInput}
-          placeholder="github-search-graphql"
-          value={searchString}
-        />
-      </label>
-      <Close className={visibility} onClick={clearInput}>
-        &times;
-      </Close>
-    </InputDiv>
-  );
-};
+const Input = ({ searchString, handleInput, clearInput }: any) => (
+  <InputDiv>
+    <label htmlFor="search">
+      <input
+        id="search"
+        type="search"
+        onChange={handleInput}
+        placeholder="github-search-graphql"
+        value={searchString}
+      />
+    </label>
+    <Close className={searchString && "show"} onClick={clearInput}>
+      &times;
+    </Close>
+  </InputDiv>
+);
 
 export default Input;
