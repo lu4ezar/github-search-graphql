28178b48ca9ce0504fca526eb93c4ba673c525b7
lu4ezar <misha-shisha@mail.ru>
2020-01-21 00:00:02 +0700

    Footer test + snapshot

---
+++ src/components/footer/Footer.test.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Footer from "./Footer";
+
+it("matches the snapshot", () => {
+  const { container } = render(<Footer />);
+  expect(container).toMatchSnapshot();
+});
+
+it("renders text link", () => {
+  const { getByText } = render(<Footer />);
+  const textLink = getByText(
+    "https://github.com/lu4ezar/github-search-graphql"
+  );
+  expect(textLink).toBeInTheDocument();
+});
+
+it("renders svg link", () => {
+  const { getByTitle } = render(<Footer />);
+  const svgLink = getByTitle("view src on Github");
+  expect(svgLink).toBeInTheDocument();
+});
