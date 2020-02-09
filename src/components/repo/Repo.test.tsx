5aff34c38d58bbeb7229e9096f745e76659852f6
lu4ezar <misha-shisha@mail.ru>
2020-02-09 12:09:24 +0700

    Repo.test: +typing, +render method, upd snapshot

--- src/components/repo/Repo.test.tsx
+++ src/components/repo/Repo.test.tsx
@@ -2,34 +2,42 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Repo from "./Repo";
+import { GetRepos_search_edges_node_Repository as RepoProps } from "../../apollo/client/__generated__/GetRepos";
 
-const repoProps = {
+const repoProps: RepoProps = {
+  __typename: "Repository",
   homepageUrl: null,
   name: "github-search-graphql",
   description: "Now with Github API v4! (graphql)",
   url: "https://github.com/lu4ezar/github-search-graphql",
-  stargazers: 0,
-  watchers: 1
+  stargazers: {
+    __typename: "StargazerConnection",
+    totalCount: 0
+  },
+  watchers: {
+    __typename: "UserConnection",
+    totalCount: 1
+  }
 };
 
+const renderComponent = ({ ...additionalProps } = {}) =>
+  render(<Repo {...repoProps} {...additionalProps} />);
+
 it("matches the snapshot", () => {
-  const { container } = render(<Repo {...(repoProps as any)} />);
+  const { container } = renderComponent();
   expect(container).toMatchSnapshot();
 });
 
 it("can show 'No description was provided' message", () => {
-  const { getByText } = render(
-    <Repo {...(repoProps as any)} description={null} />
-  );
+  const description = null;
+  const { getByText } = renderComponent({ description });
   const message = getByText("No description was provided");
   expect(message).toBeInTheDocument();
 });
 
 it("shows Homepage link", () => {
   const homepageUrl = "https://www.google.com";
-  const { getByTitle } = render(
-    <Repo {...(repoProps as any)} homepageUrl={homepageUrl} />
-  );
+  const { getByTitle } = renderComponent({ homepageUrl });
   const link = getByTitle("homepage");
   expect(link).toHaveAttribute("href", homepageUrl);
 });
