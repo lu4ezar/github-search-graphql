485144c95bef3f4e2195d04af832dd7d90f2abc6
lu4ezar <misha-shisha@mail.ru>
2020-03-03 13:22:29 +0700

    Refactor: Recent.test

--- src/components/recent/Recent.test.tsx
+++ src/components/recent/Recent.test.tsx
@@ -4,52 +4,57 @@ import { render, fireEvent } from "@testing-library/react";
 import Recent from "./Recent";
 import "jest-styled-components";
 
-const recentProps = {
-  searchHistory: ["abc", "def", "ghi"],
-  getSearchStringFromHistory: jest.fn()
-};
+interface TestRecentProps {
+  searchHistory?: string[];
+  getSearchStringFromHistory?: () => void;
+}
 
-const recentPropsNoData = {
-  searchHistory: [],
-  getSearchStringFromHistory: jest.fn()
+const renderRecent = (props?: TestRecentProps) => {
+  const utils = render(
+    <Recent
+      searchHistory={["abc", "def", "ghi"]}
+      getSearchStringFromHistory={jest.fn()}
+      {...props}
+    />
+  );
+  return { ...utils };
 };
-
-const renderComponent = () => render(<Recent {...recentProps} />);
-const renderComponentNoData = () => render(<Recent {...recentPropsNoData} />);
 
 it("matches the snapshot", () => {
-  const { container } = renderComponent();
+  const { container } = renderRecent();
   expect(container).toMatchSnapshot();
 });
 
 it("renders list of recent searches", () => {
-  const { getAllByTitle } = renderComponent();
+  const { getAllByTitle } = renderRecent();
   const listItems = getAllByTitle(/search for/i);
   expect(listItems).toHaveLength(3);
 });
 
 it("calls getSearchStringFromHistory prop function on List Item click", () => {
-  const { getByText } = renderComponent();
+  const getSearchStringFromHistory = jest.fn();
+  const { getByText } = renderRecent({
+    getSearchStringFromHistory
+  });
   const listItem = getByText("abc");
   fireEvent.click(listItem);
-  expect(recentProps.getSearchStringFromHistory).toBeCalledTimes(1);
-  fireEvent.keyDown(listItem);
-  expect(recentProps.getSearchStringFromHistory).toBeCalledTimes(2);
-  fireEvent.keyDown(listItem);
-  expect(recentProps.getSearchStringFromHistory).toBeCalledTimes(3);
-  fireEvent.keyDown(listItem);
-  expect(recentProps.getSearchStringFromHistory).toBeCalledTimes(4);
+  expect(getSearchStringFromHistory).toBeCalledTimes(1);
+  expect(getSearchStringFromHistory).toBeCalledWith("abc");
 });
 
 it("calls getSearchStringFromHistory prop function on List Item key press", () => {
-  const { getByText } = renderComponent();
-  const listItem = getByText("abc");
+  const getSearchStringFromHistory = jest.fn();
+  const { getByText } = renderRecent({
+    getSearchStringFromHistory
+  });
+  const listItem = getByText("def");
   fireEvent.keyDown(listItem);
-  expect(recentProps.getSearchStringFromHistory).toBeCalled();
+  expect(getSearchStringFromHistory).toBeCalledTimes(1);
+  expect(getSearchStringFromHistory).toBeCalledWith("def");
 });
 
 it("collapses list by clicking on its header", () => {
-  const { getByText, getByRole } = renderComponent();
+  const { getByText, getByRole } = renderRecent();
   const header = getByText("Search History");
   const list = getByRole("list");
   expect(list.classList.contains("collapsed")).toBe(true);
@@ -60,7 +65,7 @@ it("collapses list by clicking on its header", () => {
 });
 
 it("does not show 'Collapse/Expand' button on desktop", () => {
-  const { getByTitle } = renderComponent();
+  const { getByTitle } = renderRecent();
   const icon = getByTitle(/expand/i);
   // @ts-ignore
   expect(icon).toHaveStyleRule({
@@ -71,13 +76,15 @@ it("does not show 'Collapse/Expand' button on desktop", () => {
 });
 
 it("does not show 'Collapse/Expand' button if list is empty", () => {
-  const { queryByTitle } = renderComponentNoData();
+  const { queryByTitle } = renderRecent({
+    searchHistory: []
+  });
   const icon = queryByTitle(/expand/i);
   expect(icon).toBe(null);
 });
 
 it("shows 'Collapse/Expand' button if list is not empty", () => {
-  const { getByTitle } = renderComponent();
+  const { getByTitle } = renderRecent();
   const icon = getByTitle(/expand/i);
   expect(icon.classList.contains("invisible")).toBe(false);
 });
