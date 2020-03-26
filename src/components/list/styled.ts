029ea797586b5e8d60d4d770f1445c9d799c7359
lu4ezar <misha-shisha@mail.ru>
2020-03-26 15:54:28 +0700

    update List styles

--- src/components/list/styled.ts
+++ src/components/list/styled.ts
@@ -2,14 +2,9 @@ import styled from "styled-components";
 
 const ListDiv = styled.div`
   grid-area: list;
-  overflow: auto;
-  display: flex;
-  position: relative;
-  flex-direction: column;
-  padding: 1em;
-  &.filled {
-    box-shadow: inset 0px 0px 31px #262323;
-    border-radius: 12px;
+  padding: 0 1rem !important;
+  @media (min-width: 1024px) {
+    padding: 0 0 0 1rem !important;
   }
 `;
 
